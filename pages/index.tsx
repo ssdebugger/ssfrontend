bef038c34aacbcfe3a4818e267cdb220e7d7bdd0
yashraj <yashraj0207@outlook.com>
2022-03-01 20:19:26 +0530

    changed from getServerSideProps to getStaticProps in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
 import Homepage from '@/components/_pages/homepage'
 
 export default Homepage
 
-export async function getServerSideProps() {
+export const getStaticProps: GetStaticProps = async () => {
     const responseoffers = await fetch(
         'https://wpsqswbxjj.execute-api.us-east-2.amazonaws.com/dev/gettopoffers'
     )
-  
+
     const responsebundles = await fetch(
         'https://wpsqswbxjj.execute-api.us-east-2.amazonaws.com/dev/getbundleproducts'
     )
+
     const offers = await responseoffers.json()
     const bundles = await responsebundles.json()
+
     return {
         props: {
             offers,
             bundles,
         },
+        revalidate: 10,
     }
 }
