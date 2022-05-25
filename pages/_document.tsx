898c0e68243088b9d53c8a19ea4f31ef30ea261e
Gokul <fsd1@sellsage.com>
2022-05-25 14:33:04 +0530

    Pinterest tag addition

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,24 @@ export default class MyDocument extends Document {
                             name="description"
                             content="Go plastic-free with compostable & biodegradable plates, bowls, trays, utensils, and gloves!  Elegant disposable dinnerware for wedding, party and events!"
                         ></meta>
+                        <script 
+      dangerouslySetInnerHTML={{ __html:`
+      <script>
+!function(e){if(!window.pintrk){window.pintrk = function () {
+window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+  n=window.pintrk;n.queue=[],n.version="3.0";var
+  t=document.createElement("script");t.async=!0,t.src=e;var
+  r=document.getElementsByTagName("script")[0];
+  r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+pintrk('load', '2613059152744', {em: '<user_email_address>'});
+pintrk('page');
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt=""
+  src="https://ct.pinterest.com/v3/?event=init&tid=2613059152744&pd[em]=<hashed_email_address>&noscript=1" />
+</noscript>
+      ` }}
+      />
                         <link
                             rel="apple-touch-icon"
                             sizes="180x180"
