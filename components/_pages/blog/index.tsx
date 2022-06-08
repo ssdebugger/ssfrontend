640fd511fe9a9c7731e88d2afc265cdbb2e2d903
Gokul <fsd1@sellsage.com>
2022-06-08 21:14:42 +0530

    New blog push

--- components/_pages/blog/index.tsx
+++ components/_pages/blog/index.tsx
@@ -36,6 +36,7 @@ export interface PropsData {
  * Helper functions required in blog page
  */
 function formatDate(date) {
+    
     date = new Date(date)
     var year = date.getFullYear()
     var month = (1 + date.getMonth()).toString()
