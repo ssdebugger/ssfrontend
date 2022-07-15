e37dc1c015e46eb86db9f20ae7315a787eba999a
Gokul <ssdebugger@gmail.com>
2022-07-15 11:31:50 +0530

    Google ad tag installation

--- lib/google-analytics/index.tsx
+++ lib/google-analytics/index.tsx
@@ -9,9 +9,11 @@ export const pageview = (url) => {
     window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
         page_path: url,
     })
+    window.gtag('config', 'AW-592822639');
 }
 
 // log specific events happening.
 export const event = ({ action, params }) => {
     window.gtag('event', action, params)
+    
 }
