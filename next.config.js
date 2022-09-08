81befa6e72aa39a69d1a1020eac9c74f2f6ad982
Gokul <ssdebugger@gmail.com>
2022-09-08 11:29:10 +0530

    Redirecting invalid urls to blog

--- next.config.js
+++ next.config.js
@@ -8,5 +8,20 @@ module.exports = {
     images: {
         domains: ['cdn.sanity.io','ss-compressedimages.s3.amazonaws.com','ss-compressedimages.s3.us-east-2.amazonaws.com'],
     },
+    async redirects() {
+        return [
+          {
+            source: '/2020/:path*',
+            destination: '/blog',
+            permanent: true,
+          },
+          {
+              source: '/2021/:path*',
+              destination: '/blog',
+              permanent: true,
+            },
+        ]
+      },
 }
+
 
