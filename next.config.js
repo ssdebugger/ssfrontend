6405642eed2768bfdb4b2ed8e4bd10ee8f31aef6
Gokul <ssdebugger@gmail.com>
2022-09-12 21:40:43 +0530

    Product url change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const arr1 = [
   'palm-leaf-round-bowl',
   'palm-leaf-round-plate',
@@ -55,35 +54,33 @@ for (let i = 0; i < arr1.length; i++) {
       destination: `/product/${arr2[i]}`,
       permanent: true,
   })
-
+}
 module.exports = {
-    reactStrictMode: true,
-    
-    compiler : {
-        styledComponents: true,
-    } ,
-    images: {
-        domains: ['cdn.sanity.io','ss-compressedimages.s3.amazonaws.com','ss-compressedimages.s3.us-east-2.amazonaws.com'],
-    },
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-    async redirects() {
-        return [
+  reactStrictMode: true,
+
+  compiler: {
+      styledComponents: true,
+  },
+  images: {
+      domains: [
+          'cdn.sanity.io',
+          'ss-compressedimages.s3.amazonaws.com',
+          'ss-compressedimages.s3.us-east-2.amazonaws.com',
+      ],
+  },
+  async redirects() {
+      return [
           {
-            source: '/2020/:path*',
-            destination: '/blog',
-            permanent: true,
+              source: '/2020/:path*',
+              destination: '/blog',
+              permanent: true,
           },
           {
               source: '/2021/:path*',
               destination: '/blog',
               permanent: true,
-            },
-            JSON.parse(str),
-        ]
-      },
+          },
+          
+      ]
+  },
 }
-
-
